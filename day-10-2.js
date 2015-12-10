44fa84450f66d08844d06ace04cf69e8c7576e36
Jorene René <jorenerene@gmail.com>
2015-12-10 00:54:24 -0500

    Completed Day 10 Puzzles. I need to nap during the day. 40 foggy minutes won't get me to the leaderboard ever again.

---
+++ day-10-2.js
@@ -0,0 +1,25 @@
+var input = "1113222113";
+
+var update = "";
+var repeat = input [0].toString ();
+var count = 0;
+
+for (var x = 0; x < 50; x ++) {
+  for (var i = 0; i < input.length; i ++) {
+    if (input [i] == repeat) {
+      count ++;
+    } else {
+      update += count.toString () + repeat [0];
+      repeat = input [i];
+      count = 1;
+    }
+  }
+
+  input = update + "13"; // HACK
+  update = "";
+  count = 0;
+  repeat = input [0];
+}
+
+console.log (input.length);
+alert (input.length); // 3579328
